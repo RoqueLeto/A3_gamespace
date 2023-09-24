429fbad29e2d53ef8ab3dd1e6d142d5d5d86b89c
Breno-Pimentel <brenopimentel.a.m@outlook.com>
2023-09-24 17:57:34 -0300

    Backend-SEM-TOKEN

---
+++ server/src/routes/routes.js
@@ -0,0 +1,28 @@
+const { Router } = require("express");
+const router = Router();
+const cors = require("cors");
+
+//Importando as funções da classe Controller
+const {
+  getUsers,
+  createUser,
+  getUserByID,
+  deleteUser,
+  updateUser,
+} = require("../controller/userController");
+const checkToken = require("../middlewares/auth.middleware");
+const loginUser = require("../controller/LoginController");
+
+//Rotas Públicas
+router.get("/users", getUsers);
+router.post("/auth/register", createUser);
+// router.get("/users/:id", getUserByID);
+router.delete("/users/:id", deleteUser);
+router.put("/users/:id", updateUser);
+router.post("/auth/login", loginUser);
+
+//Rotas Privadas
+router.get("/user/:id", checkToken, getUserByID); //Falta o metodo de busca por id
+
+//Exportando o metodo router para fora do arquivo
+module.exports = router;
