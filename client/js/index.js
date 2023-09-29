e6be7b43dc4b0c7b4bad704a0ca151360b15d7c0
Breno-Pimentel <brenopimentel.a.m@outlook.com>
2023-09-29 16:44:43 -0300

    Fetch

--- client/js/index.js
+++ client/js/index.js
@@ -1,40 +1,40 @@
-//##################################
-//            Variáveis
-//##################################
-const btn = document.getElementById("btn");
-const registerForm = document.getElementById("register-form");
-const emailInput = document.querySelector(".email-user-input");
-const passwordInput = document.querySelector("#password-user-input");
-const passwordConfirm = document.querySelector("#passsword-isEquals");
-const spans = document.querySelectorAll(".span-required");
+// //##################################
+// //            Variáveis
+// //##################################
 
-//##################################
-//         FUNÇOES E EVENTOS
-//##################################
+// //##################################
+// //         FUNÇOES E EVENTOS
+// //##################################
 
-//Evento do botão que recupera os valores do formulário e envia para o banco de dados
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
+// //Evento do botão que recupera os valores do formulário e envia para o banco de dados
+// btn.addEventListener("click", function (e) {
+//   e.preventDefault();
 
-  //Instanciando a clase formData
-  const formaData = new FormData(registerForm);
+//   console.log(JSON.stringify(data));
+//   //Constante que armazena a função assincrona de fetch
+//   const fetchAPI = async () => {
+//     const result = await fetch("http://localhost:3000/users", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(data),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => console.log(data));
+//   };
+//   //  fetchAPI();
 
-  //trasnforma tudo em um objeto(JSON)
-  const data = Object.fromEntries(formaData);
+//   //Execução do Fetch(Busca)
+// });
 
-  //Constante que armazena a função assincrona de fetch
-  const fetchAPI = async () => {
-    const result = await fetch("http://localhost:3000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  };
-  fetchAPI();
-
-  //Execução do Fetch(Busca)
-});
+fetch("http//localhost:3001", {
+  method: "POST",
+  body: {
+    name: "Fransico",
+    email: "Francisco@teste.com",
+    senha: "Fransico123",
+  },
+})
+  .then((res) => res.json())
+  .then((data) => console.log(data));
