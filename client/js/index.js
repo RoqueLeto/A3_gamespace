520e82bf63fbf56e20891a127659d618e08acad2
Breno-Pimentel <brenopimentel.a.m@outlook.com>
2023-09-30 09:00:32 -0300

    Index - evento submit

--- client/js/index.js
+++ client/js/index.js
@@ -1,40 +1,25 @@
-// //##################################
-// //            Variáveis
-// //##################################
+const btn = document.getElementById("register-btn");
+const email = document.getElementById("email");
+const user_name = document.getElementById("name");
+const password = document.getElementById("password");
+const password_confirmation = document.getElementById("password-check");
 
-// //##################################
-// //         FUNÇOES E EVENTOS
-// //##################################
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-// //Evento do botão que recupera os valores do formulário e envia para o banco de dados
-// btn.addEventListener("click", function (e) {
-//   e.preventDefault();
+  if (password.value === password_confirmation.value) {
+    const data = {
+      email: email.value,
+      user_name: user_name.value,
+      password: password.value,
+      password_confirmation: password_confirmation.value,
+    };
+    console.log(data);
+  } else {
+    console.error("As senhas são diferentes");
+  }
+});
 
-//   console.log(JSON.stringify(data));
-//   //Constante que armazena a função assincrona de fetch
-//   const fetchAPI = async () => {
-//     const result = await fetch("http://localhost:3000/users", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(data),
-//     })
-//       .then((response) => response.json())
-//       .then((data) => console.log(data));
-//   };
-//   //  fetchAPI();
-
-//   //Execução do Fetch(Busca)
-// });
-
-fetch("http//localhost:3001", {
-  method: "POST",
-  body: {
-    name: "Fransico",
-    email: "Francisco@teste.com",
-    senha: "Fransico123",
-  },
-})
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+fetch(){
+  
+}
